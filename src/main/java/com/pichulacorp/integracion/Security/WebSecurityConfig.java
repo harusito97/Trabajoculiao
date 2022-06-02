2fe0fde38f7a9004a02ef8693ecfe23bc8e7ba8d
Juan Canales <juanignacioc@microsoft.com>
2022-06-02 00:06:11 -0400

    Arregla logout

--- src/main/java/com/pichulacorp/integracion/Security/WebSecurityConfig.java
+++ src/main/java/com/pichulacorp/integracion/Security/WebSecurityConfig.java
@@ -1,19 +1,18 @@
 package com.pichulacorp.integracion.Security;
 
-import com.pichulacorp.integracion.UserProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.pichulacorp.integracion.UserProvider;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -33,7 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .logoutRequestMatcher(new AntPathRequestMatcher("/Logout"))
+                .logoutSuccessUrl("/Login");
     }
 
     @Override
